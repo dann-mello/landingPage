d8b0fdb917a1ee7cd3d0bd5df927fd066c7662a9
Daniel Melo <o.danielworkmelo@gmail.com>
2022-12-03 20:35:16 +0000

    background header

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function menuHam(){
+    if(document.querySelector('.drop nav ul').style.display == 'none'){
+        document.querySelector('.drop nav ul').style.display = 'block';
+    }else{
+        document.querySelector('.drop nav ul').style.display = 'none';
+    }
+}
